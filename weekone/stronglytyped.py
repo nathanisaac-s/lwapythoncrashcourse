60f513e914933feaef3ef59f75cb96b24eba43eb
nathan <nathan.shurte@gmail.com>
2015-09-20 19:17:59 -0400

    new module showing typing
    inspired by https://i.imgur.com/6aclmM6.png
    
    probably too much in one bit but that's okay

---
+++ weekone/stronglytyped.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python3
+# https://i.imgur.com/6aclmM6.png
+import time
+
+
+def run_safely(potentially_broken_function):
+    # python is newer than 1980 so it has exceptions. syntax is, of course,
+    # whitespace sensitive
+    try:
+        # Java may be the industry leader getting books about OOP printing,
+        # but Python (and lots of other langs) have a stronger object model.
+        #
+        # in Python, almost everything is an object (and the things that 
+        # aren't, like infix operators, are usually sugar around the object
+        # data model.
+        #
+        # This means that methods and functions are also objects, allowing
+        # them first-class status: anything you do with an object can be
+        # done with a function. Cf. with Java prior to 1.8
+        #
+        # tl;dr: we can pass a method as a function.
+        potentially_broken_function()
+    except Exception as e:
+        print("caught exception: {exc}\n  {msg}".format(exc=type(e), msg=e))
+    # while not often used, python try-catch features an `else` that runs
+    # prior to `finally` if the block doesn't throw an error.
+    else:
+        print("you ran a function ({}) successfully. programming is hard.".format(potentially_broken_function))
+    finally:
+        print("ran {}.".format(potentially_broken_function))
+
+
+def run_an_eval(eval_str):
+    try:
+        # lol code injection on purpose
+        eval(eval_str)
+    except Exception as e:
+        print("caught exception: {exc}\n  {msg}".format(exc=type(e), msg=e))
+    else:
+        print("you did stupid shit and evaled `{}` successfully. programming is hard.".format(eval_str))
+    finally:
+        print("ran {}.".format(eval_str))
+
+
+def main():
+    # python has had lambdas since about 2003 or so, which means that at least
+    # it caught up with 1950s comp sci.
+    #
+    # in reality it's just interpreted to an anonymous `def` by the VM, and
+    # because passing named functions is okay, use of lambda tends to be
+    # limited...
+    print("\n\n[+]    let's run some simple calls.\n")
+    simple_calls = [lambda: "5" - 3,
+                    lambda: "5" + 3,
+                    lambda: "5" - "4",
+                    lambda: "5" + str(4),  # this one works.
+                    lambda: "foo" + None]
+
+    for each in simple_calls:
+        run_safely(each)
+    # this looks like a type error, but it's not: str class implements __mul__
+    # it implements __add__, too, but doesn't cast implicitly.
+    print(79 * "-")
+
+    print("\n\n[+]    let's run some calls that take args.\n")
+    # ...for example, you can name lambdas...
+    times_three = lambda x: x * 3,
+    five_plus_minus = lambda x: "5" + x - x,
+    five_minus_plus = lambda x: "5" - x + x,
+
+    # ...or just inline it explicitly...
+    def times_three_again(arg):
+        return arg * 3
+
+    # ...you can also do that on one line:
+    def times_four(x): return x * 4
+
+    # functions defined inline in another function only exist in that scope.
+    calls_with_args = [(1, times_three),
+                       (1, five_minus_plus),
+                       (1, five_plus_minus),
+                       (1, times_three_again),
+                       (1, times_four)]
+
+    for every in calls_with_args:
+        run_safely(every)
+    print(79 * "-")
+
+    print("\n\n[+]    now let's do it Biznez Rewls style\n")
+    # don't use evals.
+    calls_to_eval = ['"x * 3',
+                     '"5" + + "5"',
+                     '"foo" + + "foo"',
+                     '"5" + - "2"',
+                     '"5 - 2"']
+
+    for each in calls_to_eval:
+        run_an_eval(each)
+    print(79 * "-")
+
+
+if __name__ == "__main__":
+    # because everything is an object, we can import inline and conditionally.
+    if time.time() > 0:
+        import sys
+    sys.exit(main())
